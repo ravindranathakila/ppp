9da8e445092e922296ab628073aafc0329050e97
Will Probert <will@probert.co.nz>
2015-11-20 13:30:37 +0000

    Added saving of ranked and removed lists to csv files

--- tests/ppp_no_overlaps.R
+++ tests/ppp_no_overlaps.R
@@ -3,6 +3,10 @@
 
 directory <- file.path('/', 'Users', 'wjmprobert', 'Projects', 'ppp')
 
+# Specify the directory in which you want to save the files 
+# (here specified as the same directory as the working directory)
+save_dir <- file.path('/', 'Users', 'wjmprobert', 'Projects', 'ppp')
+
 setwd(directory)
 
 # Import the functions to run the PPP
@@ -41,3 +45,12 @@ ppp$initially_removed
 
 # The date and time when the PPP finished
 ppp$date
+
+# Save some of the output to csv files
+outfile <- paste(ppp$date, 'removed_list.csv')
+with(ppp, write.csv(removed.spp.df, file.path(save_dir, outfile), 
+    row.names = FALSE))
+
+outfile <- paste(ppp$date, 'ranked_list.csv')
+with(ppp, write.csv(ranked_list, file.path(save_dir, outfile), 
+    row.names = FALSE))
--- tests/ppp_overlaps.R
+++ tests/ppp_overlaps.R
@@ -3,6 +3,10 @@
 
 directory <- file.path('/', 'Users', 'wjmprobert', 'Projects', 'ppp')
 
+# Specify the directory in which you want to save the files
+# (here specified as the same directory as the working directory)
+save_dir <- file.path('/', 'Users', 'wjmprobert', 'Projects', 'ppp')
+
 setwd(directory)
 
 # Import the functions to run the PPP
@@ -21,3 +25,35 @@ source(file.path(directory, "tests", "ppp_pseudo_overlap_data.R"))
 ppp <- PrioritisationII(directory, weighting,multiplier,
     set_budget, overlap.indicator, discount.rate, T, stop.iteration,
     stop.no.of.species, a.data, b.data, o.data, w.data, all.costs)
+
+# Look at some output...
+
+# Such as the list of species removed, the 'iteration' column 
+# shows the iteration of the PPP in which that species was removed
+ppp$removed.spp.df
+
+# Look at the remaining ranked species (this might be empty if all are removed)
+ppp$ranked_list
+
+# List of first year budgets for each iteration
+ppp$firstyear.budgets
+
+# List of full budgets for each iteration of the PPP
+ppp$full.budgets
+
+# Any species that were initially removed (in the first iteration)
+# because W or B or S were zero.  
+ppp$initially_removed
+
+# The date and time when the PPP finished
+ppp$date
+
+# Save some of the output to csv files
+outfile <- paste(ppp$date, 'removed_list.csv')
+with(ppp, write.csv(removed.spp.df, file.path(save_dir, outfile), 
+    row.names = FALSE))
+
+
+outfile <- paste(ppp$date, 'ranked_list.csv')
+with(ppp, write.csv(ranked_list, file.path(save_dir, outfile), 
+    row.names = FALSE))
